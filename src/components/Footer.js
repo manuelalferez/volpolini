20808e6ee752d7bc4b8fcf07649f65e8daf5bbba
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-05-14 17:16:19 +0200

    cambios minimos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import "./styles/Footer.css";
 import Location from "./Footer/Location";
 import Schedule from "./Footer/Schedule";
 import Social from "./Footer/Social";
-import { Link } from "react-router-dom";
 import heart from "../images/Footer__images/heart.svg";
 
 function Footer() {
@@ -14,7 +13,14 @@ function Footer() {
       <Social />
       <p className="Footer__firm">Volpolini &copy; 2019 </p>
       <p className="Footer__made">
-        Made with <img src={heart} width="10px " alt="Heart" className='Footer__made__heart'/> by
+        Designed and Made with{" "}
+        <img
+          src={heart}
+          width="10px "
+          alt="Heart"
+          className="Footer__made__heart"
+        />{" "}
+        by
         <a
           href="http://www.manuelalferez.com"
           target="_blank"
