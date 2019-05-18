a754582cbfdc857be6fb56d2417e1d87c94a5168
Manuel Alférez Ruiz <manuel@MacBook-Air-de-Manuel.local>
2019-05-18 20:32:31 +0200

    Agregando seguridad en los enlaces del Footer y habilitado para que se abran en una nueva página

--- src/components/Footer/Social.js
+++ src/components/Footer/Social.js
@@ -6,7 +6,7 @@ function Social() {
   return (
     <div className="Social">
       {data.map(item => (
-        <a key={item.key} className={item.className} href={item.dir} />
+        <a key={item.key} className={item.className} href={item.dir} target="_blank" rel="noopener"/>
       ))}
     </div>
   );
