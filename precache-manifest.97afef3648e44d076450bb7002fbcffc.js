dc25e2c5cf845e5ddc9a6c89a53bef98b2657d98
Manuel Alférez Ruiz <manuel@MacBook-Air-de-Manuel.local>
2019-05-18 19:54:53 +0200

    Updates

---
+++ precache-manifest.97afef3648e44d076450bb7002fbcffc.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "d189a6a132b628d43783",
+    "url": "/volpolini/static/css/main.9f64e9bd.chunk.css"
+  },
+  {
+    "revision": "d189a6a132b628d43783",
+    "url": "/volpolini/static/js/main.081419d0.chunk.js"
+  },
+  {
+    "revision": "b244ccefae4ed4d438f3",
+    "url": "/volpolini/static/js/runtime~main.10582390.js"
+  },
+  {
+    "revision": "5410e71cf38b15f99a3a",
+    "url": "/volpolini/static/js/2.966ed064.chunk.js"
+  },
+  {
+    "revision": "062fc30777d64da7772f82159dc06458",
+    "url": "/volpolini/static/media/womanDress.062fc307.jpg"
+  },
+  {
+    "revision": "752bcac385d5b8d289a8e007be8cb9ac",
+    "url": "/volpolini/static/media/womanHat.752bcac3.jpg"
+  },
+  {
+    "revision": "d59603140163ad37cce828a7633f9488",
+    "url": "/volpolini/static/media/women.d5960314.jpg"
+  },
+  {
+    "revision": "cbd6eeb2a0280afdc9231f38449d785a",
+    "url": "/volpolini/static/media/contact.cbd6eeb2.jpg"
+  },
+  {
+    "revision": "8b2f97f7283bdeb39650b0f5a7a065bd",
+    "url": "/volpolini/static/media/phone.8b2f97f7.svg"
+  },
+  {
+    "revision": "5718e5d4fc9bca38d8e4ec177248b67d",
+    "url": "/volpolini/static/media/aboutUs.5718e5d4.jpg"
+  },
+  {
+    "revision": "24fcca9b7ab70458bd29a7aacc1567b7",
+    "url": "/volpolini/static/media/heart.24fcca9b.svg"
+  },
+  {
+    "revision": "399e16c044f054d316141780dea048c1",
+    "url": "/volpolini/static/media/mj-1.399e16c0.jpeg"
+  },
+  {
+    "revision": "11eca4b70f6a17c06a6697a6c79a30bf",
+    "url": "/volpolini/static/media/mj-2.11eca4b7.jpeg"
+  },
+  {
+    "revision": "c3da1479a9f0fafb90c008a8e2c2be38",
+    "url": "/volpolini/static/media/mj-3.c3da1479.jpeg"
+  },
+  {
+    "revision": "b86ec829409513d02657a0e213696749",
+    "url": "/volpolini/static/media/mj-4.b86ec829.jpeg"
+  },
+  {
+    "revision": "7ae2eb710d6ca2a8b091cfdec5902a3e",
+    "url": "/volpolini/static/media/mj-5.7ae2eb71.jpeg"
+  },
+  {
+    "revision": "c89332b56ab4760e7e0a2bc0e204a0cc",
+    "url": "/volpolini/static/media/mj-6.c89332b5.jpeg"
+  },
+  {
+    "revision": "97c3927e1c2f94ebe6657fa51ca7e3c7",
+    "url": "/volpolini/static/media/mj-7.97c3927e.jpeg"
+  },
+  {
+    "revision": "d64680298cdfb09332295c305c086a85",
+    "url": "/volpolini/static/media/mj-8.d6468029.jpeg"
+  },
+  {
+    "revision": "1f2d7af1f09f98f61bbd7208cc7b2c7e",
+    "url": "/volpolini/static/media/mj-9.1f2d7af1.jpeg"
+  },
+  {
+    "revision": "a5f96225ca7a138e764c07679343c91f",
+    "url": "/volpolini/static/media/hm-1.a5f96225.jpeg"
+  },
+  {
+    "revision": "6bb194a0b8320c0328403f1c800032be",
+    "url": "/volpolini/static/media/hm-2.6bb194a0.jpeg"
+  },
+  {
+    "revision": "1c64aa79cfcce3b27e2f02b30e93de7d",
+    "url": "/volpolini/static/media/hm-3.1c64aa79.jpeg"
+  },
+  {
+    "revision": "18f349a047e5f7b02b4741e865bccc28",
+    "url": "/volpolini/static/media/hm-4.18f349a0.jpeg"
+  },
+  {
+    "revision": "9fd3f1a4bfb979b7be63b2a9f0fba411",
+    "url": "/volpolini/static/media/hm-5.9fd3f1a4.jpeg"
+  },
+  {
+    "revision": "b753d7c097c6da9447394ecda1385365",
+    "url": "/volpolini/static/media/hm-6.b753d7c0.jpeg"
+  },
+  {
+    "revision": "77ea3c9d11b3d937e6a9b8166c3079f8",
+    "url": "/volpolini/static/media/hm-7.77ea3c9d.jpeg"
+  },
+  {
+    "revision": "b0d391eab7644f6dbd80b5aa72caf44d",
+    "url": "/volpolini/static/media/hm-8.b0d391ea.jpeg"
+  },
+  {
+    "revision": "2e5bfe048c0a40f8364b18354e0eaaa4",
+    "url": "/volpolini/static/media/hm-9.2e5bfe04.jpeg"
+  },
+  {
+    "revision": "135f4de7384f01f3f0776ee98f23e91b",
+    "url": "/volpolini/static/media/map.135f4de7.jpg"
+  },
+  {
+    "revision": "21c00c34501c5b31fa3569c75846a587",
+    "url": "/volpolini/static/media/icomoon.21c00c34.ttf"
+  },
+  {
+    "revision": "dd6894a520d22f24e1f1e45657d0d358",
+    "url": "/volpolini/static/media/icomoon.dd6894a5.eot"
+  },
+  {
+    "revision": "87de5da0a53c1a5714db6998812c29c6",
+    "url": "/volpolini/static/media/icomoon.87de5da0.svg"
+  },
+  {
+    "revision": "f6dc0acf5158f693e8a79bd52245a9ab",
+    "url": "/volpolini/static/media/icomoon.f6dc0acf.woff"
+  },
+  {
+    "revision": "a10b785f73a648e7f01bd781cdecff8c",
+    "url": "/volpolini/index.html"
+  }
+];
